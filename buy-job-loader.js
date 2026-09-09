d07dadee17ad9b16803a317089201c273d6ccb8d
alin653 <niculaea003@gmail.com>
2026-09-09 20:39:42 +0300

    Load job purchase flow on public site

---
+++ buy-job-loader.js
@@ -0,0 +1,10 @@
+// Loader separat pentru fluxul Cumpara lucrarea.
+// Poate fi inclus in pagina fara sa modifice restul aplicatiei.
+(function(){
+  if(document.querySelector('script[data-ldf-buy-job]')) return;
+  const s=document.createElement('script');
+  s.src='buy-job.js?v=2';
+  s.defer=true;
+  s.dataset.ldfBuyJob='1';
+  document.head.appendChild(s);
+})();
