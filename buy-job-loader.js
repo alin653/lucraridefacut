3f8deac4f7bc105285459d1bc15c822d446daab8
alin653 <niculaea003@gmail.com>
2026-09-10 07:38:08 +0300

    Refresh job purchase UI script

--- buy-job-loader.js
+++ buy-job-loader.js
@@ -3,7 +3,7 @@
 (function(){
   if(document.querySelector('script[data-ldf-buy-job]')) return;
   const s=document.createElement('script');
-  s.src='buy-job.js?v=2';
+  s.src='buy-job.js?v=3';
   s.defer=true;
   s.dataset.ldfBuyJob='1';
   document.head.appendChild(s);
