4b507e09e266059e2e4b02c2a04595787314712d
alin653 <niculaea003@gmail.com>
2026-09-10 09:37:59 +0300

    Refresh purchase flow cache for launch

--- buy-job-loader.js
+++ buy-job-loader.js
@@ -3,7 +3,7 @@
 (function(){
   if(document.querySelector('script[data-ldf-buy-job]')) return;
   const s=document.createElement('script');
-  s.src='buy-job.js?v=3';
+  s.src='buy-job.js?v=4';
   s.defer=true;
   s.dataset.ldfBuyJob='1';
   document.head.appendChild(s);
